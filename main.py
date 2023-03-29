5c2de06f063dbd315a440d48bb4a0f747a80d251
khaledadrani <khaledadrani@gmail.com>
2023-03-29 16:35:48 +0000

    init: first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def do_something():
+    """
+    A dummy function for testing!
+    :return:
+    """
+    print('do something!')
+
+
+def multiply(number, muiltiplier):
+    """
+    Multiply a given number by a given multiplier.
+
+    :param number: The number to multiply.
+    :type number: int
+
+    :param muiltiplier: The multiplier.
+    :type muiltiplier: int
+    """
+
+    return number * muiltiplier
+
+
+# Call the function
+print(multiply(5, 2))
